176d40e5b5ee2bc0d8ab12fd454d21440770f42e
Sibo Wang <sibo.wang@epfl.ch>
2022-10-04 22:16:28 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='nmf_gym',
       version='0.0.1',
-      install_requires=['gym']
+      install_requires=['gym'],
+      packages=find_packages()
 )
